084befa8c17f586bfff29a6901b6dda0240469d6
Valery Baturin <Valery.Baturin@cyberprotect.ru>
2025-04-01 11:40:45 +0300

    Update mapping

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -60,8 +60,8 @@ return {
         -- second key is the lefthand side of the map
 
         -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+        L = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
+        H = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
 
         -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
